65589fde0cc57062786984e1589930ce1e2a1612
hmsmiraz <hmsmiraz64729@gmail.com>
2024-01-01 02:04:13 +0600

    added repsitories

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ async function run() {
         res.send(result);
       });
 
+      app.delete("/repositories/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await repoCollection.deleteOne(query);
+        res.send(result);
+      });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
